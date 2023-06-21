c342a34fb2f8b10ffd96f571ff0cb04917366551
Julian Bermudez <ja.bermudez10@uniandes.edu.co>
2023-06-20 23:37:21 -0500

    creating typescript and go folders for solutions

---
+++ src/golang/merge-alternately.go
@@ -0,0 +1,22 @@
+func mergeAlternately(word1 string, word2 string) string {
+	var word1Index int = 0
+	var word2Index int = 0
+
+	var result string = ""
+
+	for word1Index < len(word1) && word2Index < len(word2) {
+		result = result + string(word1[word1Index]) + string(word2[word2Index])
+
+		word1Index++
+		word2Index++
+	}
+
+	if word1Index < len(word1) {
+		result = result + string(word1[word1Index:])
+	}
+	if word2Index < len(word2) {
+		result = result + string(word2[word2Index:])
+	}
+
+	return result
+}
